bb4b121b362f298b7faa3a553793fe004da45f24
Carlos-Henreis <carlos_henreis@yahoo.com.br>
2026-03-05 09:20:34 -0300

    create homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,80 @@
+import Link from "next/link";
 import Image from "next/image";
+import { createReader } from "@keystatic/core/reader";
+import keystaticConfig from "../keystatic.config";
 
-export default function Home() {
+import DiagramSearch from "./components/DiagramSearch";
+import Accordion from "./components/Accordion";
+
+const reader = createReader(process.cwd(), keystaticConfig);
+
+
+export default async function HomePage() {
+  const diagrams = await reader.collections.diagrams.all();
+
+  const diagramsSearched = diagrams.map(({ slug, entry }) => ({
+    slug,
+    title: entry.title,
+    originalAuthor: entry.originalAuthor,
+    shortDescription: entry.shortDescription,
+    coverImage: entry.coverImage,
+  }));
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="bg-zinc-950 text-zinc-100 min-h-screen">
+      {/* Hero */}
+      <section className="max-w-6xl mx-auto px-6 pt-24 pb-16">
+        <h2 className="text-5xl md:text-6xl font-semibold tracking-tight">
+          Classic Origami Remakes
+        </h2>
+
+        <p className="mt-6 text-lg text-zinc-400 max-w-2xl leading-relaxed">
+          A curated archive of reconstructed classic and historical origami
+          diagrams, carefully redrawn and preserved.
+        </p>
+      </section>
+
+      {/* Grid */}
+      <section className="max-w-6xl mx-auto px-6 pb-24">
+        <DiagramSearch diagrams={diagramsSearched} />
+      </section>
+
+      {/* about */}
+      <section className="max-w-6xl mx-auto px-6 pb-16">
+        <h3 className="text-5xl md:text-6xl font-semibold tracking-tight pb-6">
+          About This project
+        </h3>
+
+        <Accordion
+        items={[
+          {
+            title: "What is this project?",
+            content: (
+              <p>
+                This archive contains reconstructed classic origami diagrams.
+              </p>
+            ),
+          },
+          {
+            title: "Can I download the PDF?",
+            content: (
+              <p>
+                Yes. Each diagram includes a high-resolution PDF for download.
+              </p>
+            ),
+          },
+          {
+            title: "Are these original designs?",
+            content: (
+              <p>
+                No. These are carefully redrawn remakes of historical diagrams.
+              </p>
+            ),
+          },
+        ]}
+      />
+      </section>
+
+    </main>
   );
 }
